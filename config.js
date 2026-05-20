9d0a17625c789ea506df3ca178856d1c0f55890c
Vishalya <vsooriarachchi@gmail.com>
2026-05-20 09:30:10 +0100

    Redirect local app entry to Cloudflare

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
+window.METRICFLOW_CLOUDFLARE_APP_URL = "https://metricflow-analytics.pages.dev";
 window.METRICFLOW_API_BASE_URL = "https://metricflow-api.vsooriarachchi.workers.dev";
+
+if (["localhost", "127.0.0.1", "::1"].includes(window.location.hostname)) {
+  window.location.replace(window.METRICFLOW_CLOUDFLARE_APP_URL);
+}
